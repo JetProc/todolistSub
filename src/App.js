b4809fdb02215ecf0da3162c6de00281f0fa3447
Soonjae <soonjae8297@gmail.com>
2021-12-02 18:07:18 +0900

    Add background & Set a frame

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
+import React from 'react';
+import styled from 'styled-components';
+
 import "./App.css";
+
+import backgroundImage from './images/patternedBackground.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-          Hi
-      </header>
-    </div>
+    <Container>
+      <Title>Title Here</Title>
+      <Body>Body Here</Body>
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  font-family: myFont;
+
+  display:flex;
+  flex-direction:column;
+  background-image:url(${backgroundImage});
+
+  width:100%;
+  height:100vh;
+`;
+
+const Title = styled.div`
+  height:140px;
+  line-height:140px;
+  text-align:center;
+  font-size:3em;
+`;
+
+const Body = styled.div`
+  margin:1vw;
+  font-size:2em;
+`;
